bd0998ac5ee82d08b254c2437be1af356ec850b0
KristofferHode <kristoffer9.kodehode@gmail.com>
2025-12-04 13:00:00 +0100

    Added Monsters

--- Compendium/MonsterTable.cs
+++ Compendium/MonsterTable.cs
@@ -3,8 +3,8 @@ using System.Reflection;
 public class Monsters
 {
     // Create monsters for the dungeon 
-    private string? MonsterName{get;set;}
-    private string? MonsterDescription{get;set;}
+    public string? MonsterName{get;set;}
+    public string? MonsterDescription{get;set;}
 
     public Monsters(string name, string description)
     {
@@ -12,5 +12,9 @@ public class Monsters
         MonsterDescription= description;
     }   
     public Monsters Goblin = new Monsters ("Goblin","a filthy goblin");
+    public Monsters Hobgoblin = new Monsters ("Hobgoblin"," a bigger and stronger version of its meek cousin");
+    public Monsters Gorger= new Monsters ("Gorger", "starved to madness, what the Gorger lacks in mass it more then makes up for with fierce voilence");
+    public Monsters Ogre= new Monsters ("Ogre","Brutal and always hungry. These hulking masses of muscle and fat are a force to be reckond with.");
+    
     
  }
